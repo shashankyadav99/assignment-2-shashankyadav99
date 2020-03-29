4a58087a077b5f964a0113363cc6f65fd9e937f3
Shashank Yadav <shashank.yadav_cs18@gla.ac.in>
2020-03-29 13:20:56 +0530

    Added deQueue method in MyPriorityQueue class.

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,41 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
 public class MyPriorityQueue {
+
+    private Node front;
+
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
+    public void enQueue(Node newNode) {
+        Node temp = getFront();
+        if (getFront() == null || getFront().getStudent().getRollNumber() > newNode.getStudent().getRollNumber()) {
+            setFront(newNode);
+            getFront().setNextNode(temp);
+        } else {
+            while (temp.getNextNode() != null && temp.getNextNode().getStudent().getRollNumber() <= newNode.getStudent().getRollNumber()) {
+                temp = temp.getNextNode();
+            }
+            newNode.setNextNode(temp.getNextNode());
+            temp.setNextNode(newNode);
+        }
+    }
+
+    public Node deQueue() {
+        if (getFront() == null) {
+            System.out.println("Empty queue");
+            return null;
+        }
+        Node temp = getFront();
+        setFront(getFront().getNextNode());
+        return temp;
+    }
 }
