fcdd2c273c7efdfe37397614e98455c46d6fee96
Shashank Yadav <shashank.yadav_cs18@gla.ac.in>
2020-03-29 13:13:47 +0530

    Implemented Node Class.

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,10 +1,36 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Shashank
  *  Date: 23-Mar-20
  *  Time: 7:13 PM
  */
 package problem3.node;
 
+import problem5.student.Student;
+
 public class Node {
+    private Student student;
+    private Node nextNode;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    @Override
+    public String toString() {
+        return "Node " + "\n" +
+                getStudent();
+    }
 }
