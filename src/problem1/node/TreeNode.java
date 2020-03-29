d9f205a6c1ba80b39e393af742ae53e800829d02
Shashank Yadav <shashank.yadav_cs18@gla.ac.in>
2020-03-29 12:58:59 +0530

    Added toString in TreeNode class.

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,10 +1,43 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Shashank
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode leftNode;
+    private TreeNode rightNode;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    @Override
+    public String toString() {
+        return "data ==>> " + getData();
+    }
+
 }
