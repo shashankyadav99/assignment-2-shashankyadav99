3099eca4424367a7a013cea5c7bca81b819afdc4
Shashank Yadav <shashank.yadav_cs18@gla.ac.in>
2020-03-29 13:32:29 +0530

    Completed Problem3.

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -1,11 +1,46 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Shashank
  *  Date: 23-Mar-20
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
+
+import java.util.Scanner;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        MyPriorityQueue priorityQueue = new MyPriorityQueue();
+        System.out.println("Enter number of nodes ");/*--->*/
+        int nodes = scan.nextInt();
+        while (nodes-- > 0) {
+            Student student = new Student();
+            System.out.println("Enter name ");/*--->*/
+            String name = scan.next().trim();
+            System.out.println("Enter roll number");/*--->*/
+            int roll = scan.nextInt();
+            student.setName(name);
+            student.setRollNumber(roll);
+
+            Node node = new Node();
+            node.setStudent(student);
+
+            priorityQueue.enQueue(node);
+        }
+
+        System.out.println("Displaying Queue");
+
+        Node node = priorityQueue.deQueue();
+        while (node != null) {
+            System.out.println(node);
+            node = priorityQueue.deQueue();
+        }
+    }
 }
