dfb4fec884685d8dd13a1407c4bf543636aee1a0
Shashank Yadav <shashank.yadav_cs18@gla.ac.in>
2020-03-29 13:42:27 +0530

    Updated MyQueue class.

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,11 +1,57 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Shashank
  *  Date: 23-Mar-20
  *  Time: 7:11 PM
  */
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem4.node.Node;
+
 public class MyQueue {
+
+    private Node front;
+    private Node rear;
+
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
+    public Node getRear() {
+        return rear;
+    }
+
+    public void setRear(Node rear) {
+        this.rear = rear;
+    }
+
+    public void enQueue(Node newNode) {
+        if (front == null && rear == null) {
+            setFront(newNode);
+            setRear(newNode);
+        } else {
+            getRear().setNext(newNode);
+            setRear(getRear().getNext());
+        }
+    }
+
+    public Node deQueue() {
+        Node temp;
+        if (getFront() == null) {
+            return null;
+        } else if (getFront().getNext() == null) {
+            temp = getFront();
+            setFront(null);
+            setRear(null);
+        } else {
+            temp = getFront();
+            setFront(getFront().getNext());
+        }
+        return temp;
+    }
 }
