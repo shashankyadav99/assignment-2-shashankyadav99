4693d150bebc89ada03464308a09f75f92f4a4a3
Shashank Yadav <shashank.yadav_cs18@gla.ac.in>
2020-03-29 13:56:04 +0530

    Completed node class.

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,10 +1,31 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Shashank
  *  Date: 23-Mar-20
  *  Time: 7:06 PM
  */
 package problem5.node;
-// to define problem4.node properties
+
+import problem5.student.Student;
+
+// to define node properties
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
